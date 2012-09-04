0813ebde61a86181580df8bdd2c4d95b50ac065f
Rémy.Baranx <remy.gv@gmail.com>
2012-09-04 17:57:11 -0500

    nettoyage du plugin gvisegoria

--- mod/gvisegoria/start.php
+++ mod/gvisegoria/start.php
@@ -3,8 +3,6 @@
  * GV isegoria plugin
  *
  */
-	require_once(dirname(__FILE__) . "/lib/isegoria.php");
- 
  
 elgg_register_event_handler('init', 'system', 'gvisegoria_init');
 
@@ -26,25 +24,11 @@ function gvisegoria_init() {
     'text' => elgg_echo('gvgroups:isegoria'),
     'priority' => '10',
     ));
-
-    // add an admin menu to update old users with default dashboard widgets
-	elgg_register_admin_menu_item('administer', 'update_old_users', 'isegoria');
-
-    // add default dashboard widgets for new users
-    elgg_register_event_handler("create", "user", "isegoria_new_user");
-
-    // register some new actions
-	$action_base = elgg_get_plugins_path() . 'gvisegoria/actions/';
-    elgg_register_action("admin/update_old_users", "$action_base/admin/update_old_users.php");
 }
 
-function isegoria_new_user($hook, $type, $user) {
-    if(!empty($user) && ($user instanceof ElggUser)){
-        isegoria_add_default_widget($user->guid);
-    }
-}
-
-// isegoria page handler
+/**
+ * GV isegoria page handler.
+ */
 function isegoria_page_handler() {
 	// Ensure that only logged-in users can see this page
 	gatekeeper();
@@ -54,11 +38,7 @@ function isegoria_page_handler() {
 	elgg_set_page_owner_guid(elgg_get_logged_in_user_guid());
 	$title = elgg_echo('dashboard');
 
-	// wrap intro message in a div
-//	$intro_message = elgg_view('dashboard/blurb');
-
 	$params = array(
-//		'content' => $intro_message,
 		'num_columns' => 3,
 		'show_access' => false,
 	);
@@ -69,4 +49,3 @@ function isegoria_page_handler() {
 	echo elgg_view_page($title, $body);
 	return true;
 }
-
