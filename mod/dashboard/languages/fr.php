1d12b6d5769e8c902ef359cc58f89dee76fe4a96
Rémy.Baranx <remy.gv@gmail.com>
2012-09-05 16:14:37 -0500

    traduction du module Dashboard

---
+++ mod/dashboard/languages/fr.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User dashboard languages
+ */
+
+$french = array(
+	'dashboard:widget:group:title' => 'Activité d\'un groupe',
+	'dashboard:widget:group:desc' => 'Voir l\'activité d\'un de vos groupes',
+	'dashboard:widget:group:select' => 'Choisissez un groupe',
+	'dashboard:widget:group:noactivity' => 'Il n\'y a aucune activité dans ce groupe',
+	'dashboard:widget:group:noselect' => 'Editer ce widget pour choisir le groupe à suivre',
+);
+
+add_translation("fr", $french);
