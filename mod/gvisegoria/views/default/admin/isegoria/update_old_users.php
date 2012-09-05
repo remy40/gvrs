ac54443df4ad431575953187f5fef5d9ce55845c
Rémy.Baranx <remy.gv@gmail.com>
2012-09-04 19:14:57 -0500

    suppression du menu admin pour mettre à jour le dashboard des anciens utilisateurs

--- mod/gvisegoria/views/default/admin/isegoria/update_old_users.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo elgg_view_form('admin/update_old_users');
-    
--- mod/gvisegoria/views/default/forms/admin/update_old_users.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $form_body = "<div>";
-    $form_body .= "<p>" . elgg_echo('gvisegoria:update_old_users:explanation') . "</p>";
-    $form_body .= elgg_view('input/submit', array('value' => elgg_echo('gvisegoria:update_old_users')));
-    $form_body .= "</div>";
-
-    echo $form_body;
