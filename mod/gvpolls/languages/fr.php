2343353e41053a0630cf17298c25eb9c144b3ee7
Rémy.Baranx <remy.gv@gmail.com>
2012-09-12 21:58:11 -0500

    ajout de choix multiples pour les sondages

---
+++ mod/gvpolls/languages/fr.php
@@ -0,0 +1,9 @@
+<?php
+
+	$french = array(
+        'gvpolls:multiple_choices_label' => "Autoriser plusieurs réponses pour un même utilisateur"
+	);
+					
+	add_translation("fr",$french);
+
+?>
--- mod/gvpolls/start.php
+++ mod/gvpolls/start.php
@@ -9,5 +9,12 @@ elgg_register_event_handler('init', 'system', 'gvpolls_init');
  * Initialize the GV polls plugin.
  */
 function gvpolls_init() {
+    
+    // override polls library
+	elgg_register_library('elgg:polls', elgg_get_plugins_path() . 'gvpolls/models/model.php');
 
+    // override polls actions
+	$action_path = elgg_get_plugins_path() . 'gvpolls/actions/polls';
+	elgg_register_action("polls/edit","$action_path/edit.php");
+	elgg_register_action("polls/vote","$action_path/vote.php");
 }
