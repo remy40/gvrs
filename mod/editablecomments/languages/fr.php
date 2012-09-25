9637f4c0c1bcae11ec462d51436f2a0c273c6088
Rémy.Baranx <remy.gv@gmail.com>
2012-09-24 22:14:01 -0500

    ajout de traductions

---
+++ mod/editablecomments/languages/fr.php
@@ -0,0 +1,7 @@
+<?php
+$language = array (
+  'comment:error' => 'Erreur d\'enregistrement du commentaire',
+  'comment:edited' => 'commentaire modifié',
+  'comment:edit' => 'Modifier le commentaire',
+);
+add_translation("fr", $language);
---
+++ mod/friend_request/languages/fr.php
@@ -0,0 +1,38 @@
+<?php
+$language = array (
+  'friend_request' => 'Demande d\'amitié',
+  'friend_request:menu' => 'Demandes d\'amitié',
+  'friend_request:title' => 'Demande d\'amitié pour: %s',
+  'friend_request:new' => 'Nouvelle demande d\'amitié',
+  'friend_request:friend:add:pending' => 'Demande d\'amitié en attente',
+  'friend_request:newfriend:subject' => '%s veut être votre ami!',
+  'friend_request:newfriend:body' => '%s veut être votre ami! Mais ils attendent que vous acceptiez leur requête ... 
+
+Vous pouvez voir vos demandes d\'amitié en attente ici:
+%s
+
+Vérifier que vous êtes bien identifié avant de cliquer sur le lien suivant, sinon vous serez redirigé vers la page d\'identification.
+
+(Vous ne pouvez pas répondre à cet email.)',
+  'friend_request:add:failure' => 'Désolé, à cause d\'une erreur système, nous sommes dans l\'impossibilité de valider votre requête. Veuillez réessayer.',
+  'friend_request:add:successful' => 'Vous avez envoyé une demande d\'amitié à %s. Ils doivent approuver votre demande avant que la relation d\'amitié soit effective.',
+  'friend_request:add:exists' => 'Vous avez déjà envoyé une demande d\'amitié à %s.',
+  'friend_request:approve' => 'Approuver',
+  'friend_request:approve:successful' => '%s est désormais un de vos amis.',
+  'friend_request:approve:fail' => 'Erreur durant la création de la relation d\'amitié avec %s',
+  'friend_request:decline' => 'Décliner',
+  'friend_request:decline:subject' => '%s a décliné votre demande d\'amitié',
+  'friend_request:decline:message' => 'Cher %s,
+
+%s a décliné votre demande d\'amitié.',
+  'friend_request:decline:success' => 'Demande d\'amitié déclinée avec succès.',
+  'friend_request:decline:fail' => 'Erreur durant le refus d\'une demande d\'amitié, veuillez réessayer.',
+  'friend_request:revoke' => 'Révoquer',
+  'friend_request:revoke:success' => 'Demande d\'amitié révoquée avec succès',
+  'friend_request:revoke:fail' => 'Erreur de révocation de la demande d\'amitié, veuillez réessayer',
+  'friend_request:received:title' => 'Demandes d\'amitié reçues',
+  'friend_request:received:none' => 'Aucune demande d\'amitié en attente de votre approbation',
+  'friend_request:sent:title' => 'Demandes d\'amitié envoyées',
+  'friend_request:sent:none' => 'Aucune demande d\'amitié en attente d\'approbation',
+);
+add_translation("fr", $language);
