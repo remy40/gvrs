407b8f256383b8bfb672d75b6e5ce67f0425d4e0
Rémy.Baranx <remy.gv@gmail.com>
2012-09-25 16:46:10 -0500

    ajout traduction microblog

---
+++ mod/gvthewire/languages/fr.php
@@ -0,0 +1,11 @@
+<?php
+
+	$french = array(
+        'gvthewire:enablemicroblog' => "Activer le microblogging pour le groupe",
+        'gvthewire:group' => "microblog de groupe",
+        'thewire:group' => "Microblog du groupe %s",
+    );
+    
+	add_translation("fr", $french);
+
+?>
