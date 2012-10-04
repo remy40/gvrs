826854fee3f017109d0a78a4c01b6f670281d048
Rémy.Baranx <remy.gv@gmail.com>
2012-10-04 07:49:30 -0500

    finalisation widgets

---
+++ mod/gvwidgets/views/default/widgets/group_new_members/content.php
@@ -0,0 +1,34 @@
+<?php
+
+    $limit = $entity->num_display;
+        
+    //if no number has been set, default to 5
+    if(!$limit) $limit = 5;
+
+    if (elgg_in_context('groups')) {
+        $group = elgg_get_page_owner_entity();
+
+        if ($group && ($group instanceof ElggGroup)) {
+            $content = elgg_list_entities_from_relationship(array(
+                'relationship' => 'member',
+                'relationship_guid' => $group->guid,
+                'inverse_relationship' => true,
+                'types' => 'user',
+                'limit' => $limit,
+            ));
+
+            echo $content;
+            
+            if ($content) {
+                $more_link = elgg_view('output/url', array(
+                    'href' => "groups/members/".$group->guid,
+                    'text' => elgg_echo("groups:members:more"),
+                    'is_trusted' => true,
+                ));
+                echo "<span class=\"elgg-widget-more\">$more_link</span>";
+            } else {
+                echo elgg_echo("groups:members:none");
+            }
+        }
+    }
+
