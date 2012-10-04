e69899201773af4b96f0bc360e068299b6e3857c
Rémy.Baranx <remy.gv@gmail.com>
2012-10-04 11:09:48 -0500

    redirect to dashboard after login

--- mod/loginrequired/index.php
+++ mod/loginrequired/index.php
@@ -6,6 +6,11 @@
  * @subpackage Core
  */
 
-$body = elgg_view_layout('one_page_login');
-// use our own page shell
-echo elgg_view_page('', $body, 'one_page_login');
+if (elgg_is_logged_in()) {
+	forward('dashboard');
+}
+else {
+    $body = elgg_view_layout('one_page_login');
+    // use our own page shell
+    echo elgg_view_page('', $body, 'one_page_login');
+}
