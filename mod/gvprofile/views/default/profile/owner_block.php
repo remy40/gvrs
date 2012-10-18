b596f11b64efdaf186b3500f830e1368a52575a1
Rémy.Baranx <remy.gv@gmail.com>
2012-10-18 11:07:39 -0500

    ajout du menu admin pour le profil utilisateur

--- mod/gvprofile/views/default/profile/owner_block.php
+++ mod/gvprofile/views/default/profile/owner_block.php
@@ -21,6 +21,7 @@ $menu = elgg_trigger_plugin_hook('register', "menu:user_hover", array('entity' =
 $builder = new ElggMenuBuilder($menu);
 $menu = $builder->getMenu();
 $actions = elgg_extract('action', $menu, array());
+$admin = elgg_extract('admin', $menu, array());
 
 $profile_actions = '';
 if (elgg_is_logged_in() && $actions) {
@@ -31,11 +32,28 @@ if (elgg_is_logged_in() && $actions) {
 	$profile_actions .= '</ul>';
 }
 
+// if admin, display admin links
+$admin_links = '';
+if (elgg_is_admin_logged_in() && elgg_get_logged_in_user_guid() != elgg_get_page_owner_guid()) {
+	$text = elgg_echo('admin:options');
+
+	$admin_links = '<ul class="profile-admin-menu-wrapper">';
+	$admin_links .= "<li><a rel=\"toggle\" href=\"#profile-menu-admin\">$text&hellip;</a>";
+	$admin_links .= '<ul class="profile-admin-menu" id="profile-menu-admin">';
+	foreach ($admin as $menu_item) {
+		$admin_links .= elgg_view('navigation/menu/elements/item', array('item' => $menu_item));
+	}
+	$admin_links .= '</ul>';
+	$admin_links .= '</li>';
+	$admin_links .= '</ul>';	
+}
+
 echo <<<HTML
 
 <div id="profile-owner-block">
 	$icon
 	$profile_actions
+	$admin_links
 </div>
 
 HTML;
