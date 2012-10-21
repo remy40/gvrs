8fa97e033c9d94d6d520c54e724673c35b9b21ea
Rémy.Baranx <remy.gv@gmail.com>
2012-10-21 15:30:47 -0500

    nouveau fichier de traduction

---
+++ mod/chat/vendors/freichat/lang/french.php
@@ -0,0 +1,67 @@
+<?php
+//FreiChatX - English language
+//English is the primary language
+//cb->chatbox
+//g->guest
+//Do not use space but nbsp;
+//If you have copied this file and you are making your own
+//language then please write in the format similar to
+//english_informal.php
+
+
+//NOTE: &nbsp; is euivalent to a space 
+
+    $frei_trans['cb_head']           =          'Utilisateurs';
+    $frei_trans['g_prefix']          =          'Invité-';
+    $frei_trans['pwdby']             =          'Powered By';
+    $frei_trans['noline']            =          'Aucun connecté';
+    $frei_trans['noperms']           =          'Vous devez vous connecter au chat!';
+    $frei_trans['on_Hors ligne']        =          'Initialisation...';
+    $frei_trans['go_En ligne']         =          'Etre En ligne';
+    $frei_trans['go_Hors ligne']        =          'Etre Hors ligne';
+    $frei_trans['go_invisible']      =          'Etre Invisible';
+    $frei_trans['go_busy']           =          'Etre Occupé';
+    $frei_trans['newmesg']           =          'Nouveau Message de';
+    $frei_trans['restore_drag_pos']  =          'Remettre les fenêtres dans leurs positions initiales';
+    $frei_trans['status_txt']        =          'Modifier son statut';
+    $frei_trans['opt_txt']           =          'Options';
+    $frei_trans['onHors lignemesg']     =          'Cliquer pour chatter!';
+
+    $frei_trans['plugin_transdisable']  =       'Désactiver';
+    $frei_trans['plugin_trans_orig']    =       'Message original:';
+
+    $frei_trans['titles_translate']     =       'Traduire dans son langage';
+    $frei_trans['titles_clrcht']     =       'Effacer l\'historique';
+    $frei_trans['titles_upload']        =       'Envoyer un ficher';
+    $frei_trans['titles_smiley']        =       'Insérer un smiley';
+    $frei_trans['titles_save']        =       'Sauver l\'historiques';
+    $frei_trans["titles_mail"] 		=       'Envoyer un email de conversation';
+
+    $frei_trans['status_En ligne']        =       'Vous êtes en ligne';
+    $frei_trans['status_busy']          =       'Vous êtes occupé';
+    $frei_trans['status_invisible']     =       'Vous êtes invisible';
+    $frei_trans['status_Hors ligne']       =       'Vous êtes hors ligne';
+    $frei_trans['default_status']       =       'Je suis disponible';
+
+    $frei_trans['status_txt_En ligne']        =       'Disponible';
+    $frei_trans['status_txt_busy']          =       'Occupé';
+    $frei_trans['status_txt_invisible']     =       'Invisible';
+    $frei_trans['status_txt_Hors ligne']       =       'Hors ligne';
+    
+    $frei_trans['chatHistoryNotFound']  =       'Historique non trouvé!';
+    $frei_trans['chatHistoryDeleted']   =       'Historique nettoyé';
+
+    $frei_trans['set_custom_mesg']   =       'Définir un message personnaliser:';
+    
+    $frei_trans['chat_room_title'] = 'Salons';
+    
+    $frei_trans['chat_message_me'] = 'Moi';
+    
+    $frei_trans['chat_status'] = 'L\'utilisateur est hors-ligne. Les messages lui seront remis lorsqu\'il sera à nouveau en ligne. ';
+    
+    $frei_trans['rtl'] = 'Basculer vers le texte rtl';
+    
+    $frei_trans['profilelink'] = 'Voir profil';
+    
+         return 1;
+?>
