5aed5e400418243556e9509f71f50be7fbf94e37
Rémy.Baranx <remy.gv@gmail.com>
2012-10-22 17:00:43 -0500

    suppression du chat

--- mod/chat/languages/fr.php
+++
@@ -1,7 +0,0 @@
-<?php
-$language = array (
-  'chat:step2' => 'Avez-vous installé le chat ? Activer le chat seulement après avoir terminé l\'installation',
-  'chat:admin' => 'Accès au panel d\'administration du chat',
-  'chat:install' => 'Installer le chat',
-);
-add_translation("fr", $language);
