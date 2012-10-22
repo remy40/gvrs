d2bd2a31fec3965520c560d32d222743debc2441
Rémy.Baranx <remy.gv@gmail.com>
2012-10-22 17:55:02 -0500

    accès aux widgets même pour un groupe privé

---
+++ mod/gvgroups/views/default/groups/profile/layout.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Layout of the groups profile page
+ *
+ * @uses $vars['entity']
+ */
+
+echo elgg_view('groups/profile/summary', $vars);
+echo elgg_view('groups/profile/widgets', $vars);
