2f81bd88730e93c5aedb9c94bee1083e4a39d1db
Rémy.Baranx <remy.gv@gmail.com>
2012-10-23 09:13:06 -0500

    correction system_message pour login

--- mod/loginrequired/views/default/page/one_page_login.php
+++ mod/loginrequired/views/default/page/one_page_login.php
@@ -4,6 +4,8 @@
 */
 // Set the content type
 header("Content-type: text/html; charset=UTF-8");
+
+	$messages = elgg_view('page/elements/messages', array('object' => $vars['sysmessages']));
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
@@ -11,6 +13,9 @@ header("Content-type: text/html; charset=UTF-8");
 <?php echo elgg_view('page/elements/head', $vars); ?>
 </head>
 <body>
-<?php echo $vars['body']; ?>
+	<div class="elgg-page-messages">
+		<?php echo $messages; ?>
+	</div>
+	<?php echo $vars['body']; ?>
 </body>
 </html>
